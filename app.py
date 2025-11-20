69af8b48f139d4bb6754ec15aeac253002a9bf0e
Basavaraj <basavaraj-hm@users.noreply.github.com>
2025-11-20 17:39:15 +0530

    Update app.py ddddd

--- app.py
+++ app.py
@@ -5,10 +5,29 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ["http://quotes.toscrape.com"]
 
     def parse(self, response):
-        FastAPI()
+        for quote in response.css("div.quote"):
+           .crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from quotes_spider import QuotesSpider
 
-@app.get("/quotes")
-def get_quotes():
-    with open("quotes.json", "r") as f:
-        data = json.load(f)
-    return {"quotes": data}
+app = FastAPI()
+
+@app.get("/scrape")
+def scrape_quotes():
+    scraped_data = []
+
+    # Define a custom pipeline to collect data
+    class CollectPipeline:
+        def process_item(self, item, spider):
+            scraped_data.append(item)
+            return item
+
+    process = CrawlerProcess(settings={
+        "LOG_ENABLED": False,
+        "ITEM_PIPELINES": { '__main__.CollectPipeline': 1 }
+    })
+
+    process.crawl(QuotesSpider)
+    process.start()  # Blocks until spider finishes
+
+    return {"quotes": scraped_data}
